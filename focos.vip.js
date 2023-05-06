1556ecdde5a67f92918468c3bf170db23b424e8d
oneopc <965587913@qq.com>
2023-05-06 10:06:07 +0800

    Update focos.vip.js

--- focos.vip.js
+++ focos.vip.js
@@ -18,8 +18,7 @@
 [rewrite_local]
   
 # > focos☆解锁会员权限（2023-05-05）@ddgksf2013
-^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body focos.vip.js 
-
+^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/oneopc/learngit/master/focos.vip.js
 
 [mitm]
  
